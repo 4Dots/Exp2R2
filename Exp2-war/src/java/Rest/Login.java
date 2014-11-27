c7b927fa5beceeb1e97dfe31c765318dde60830d
estudiante <estudiante@ISIS2503-27.virtual.uniandes.edu.co>
2014-11-27 13:48:39 -0500

    huehuehue

--- Exp2-war/src/java/Rest/Login.java
+++ Exp2-war/src/java/Rest/Login.java
@@ -9,7 +9,14 @@ package Rest;
 import Beans.LoginBean;
 import Beans.Metodos;
 import bos.Usuario;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -43,20 +50,73 @@ public class Login {
      */
     @GET
     @Produces("application/json")
-    public String getJson(@QueryParam("id") String id, @QueryParam("nombre") String nombre) {
+    public String getJson(@QueryParam("id") String id, @QueryParam("nombre") String nombre, @QueryParam("ref") String ref, @QueryParam("token") String token) {
         //TODO return proper representation object
-        System.out.println("ID: "+ id);
-        System.out.println("Nombre: " + nombre);
-      
-        Metodos met = new Metodos();
-        ArrayList amigos =  met.buscarAmigosFB(id);
-        ArrayList likes = met.buscarLikesFB(id);
-        Usuario u = new Usuario(likes, id, nombre, amigos);
-        met.guardarUsuario(u);
-        Usuario usu = LoginBean.getInstance().conseguirUsuario();
-        System.out.println("Usuario creado: "+usu.getID());
-        System.out.println("Llego a REST");
-        return "true";
+        
+        if (ref.equals("fb"))
+        {
+            System.out.println("ID: "+ id);
+            System.out.println("Nombre: " + nombre);
+            
+            Metodos met = new Metodos();
+            ArrayList amigos =  met.buscarAmigosFB(id);
+            ArrayList likes = met.buscarLikesFB(id);
+            Usuario u = new Usuario(likes, id, nombre, amigos);
+            met.guardarUsuario(u);
+            Usuario usu = LoginBean.getInstance().conseguirUsuario();
+            System.out.println("Usuario creado: "+usu.getID());
+            System.out.println("Llego a REST");
+            
+            String res = "[";
+            
+            for (int i = 0; i < likes.size(); i++)
+            {
+                res += "\"\"" + likes.get(i) + "\"\"";
+                if (i < likes.size()-1)
+                    res += ",";
+            }
+            res += "]";
+            return res;
+        }
+        else if (ref.equals("g"))
+        {
+            String url = "http://www.google.com/search?q=mkyong";
+            
+            URL obj;
+            try 
+            {
+                obj = new URL(url);
+                 HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            
+            // optional default is GET
+            con.setRequestMethod("GET");
+            
+            //add request header
+            //con.setRequestProperty("User-Agent", USER_AGENT);
+            
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+            
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            
+            //print result
+            return response.toString();
+            } catch (Exception ex) {
+                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+            }
+             
+        }
+        return "ERR";
+        
     }
 
     /**
